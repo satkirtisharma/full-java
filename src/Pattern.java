e49849d930fd96cf2dd406d60e7f82bb8ae24cb7
Satkirti_Sharma <sksharma5864@gmail.com>
2023-04-09 19:24:36 +0530

    CREATED

--- src/Pattern.java
+++ src/Pattern.java
@@ -22,24 +22,137 @@ public class Pattern {
 //            System.out.println();
 //        }
         //print character patter
-        char c='A';
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(c);
-                c++;
+//        char c='A';
+//        for (int i = 1; i <= x; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(c);
+//                c++;
+//            }
+//           // c='A';
+//            System.out.println();
+//        }
+//        //print character patter
+//         c='A';
+//        for (int i = 1; i <= x; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(c);
+//                c++;
+//            }
+//            c='A';
+//            System.out.println();
+//        }
+        //hollow rectangle pattern
+//
+//        //Inverted and rotate half pyramid
+//        x=5;
+//        for (int i = 0; i <x; i++) {
+//            for (int j = 0; j <x; j++) {
+//                if(i+j>=x-1){
+//                    System.out.print("*");
+//                }else System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+//        //INVERTED HALF PYRAMID WITH NUMBERS
+//        for (int i = 0; i <5; i++) {
+//            for (int j =1; j <=5-i; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//        //floyd's Triangle
+//        x=7;
+//        int n=1;
+//        for (int i = 0; i <x; i++) {
+//            for (int j = 1; j <= x-i ;j++) {
+//                System.out.print(n+" ");
+//                n++;
+//            }
+//            System.out.println();
+//        }
+//        //0-1 Triangle
+//        x=5;
+//        for (int i = 1; i <=x; i++) {
+//            for (int j = 1; j <= i ;j++) {
+//                if ((i+j)%2==0) System.out.print("1");
+//                else System.out.print("0");
+//            }
+//            System.out.println();
+//        }
+//        //Butterfly Pattern
+//        x=4;
+//        for (int i = 1; i <= x; i++) {
+//            for (int j = 1; j <=i; j++) {
+//                System.out.print("* ");
+//            }
+//            for (int j = 1; j <= 2*(x-i); j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 1; j <=i ; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = x; i >= 1; i--) {
+//            for (int j = 1; j <=i; j++) {
+//                System.out.print("* ");
+//            }
+//            for (int j = 1; j <= 2*(x-i); j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 1; j <=i ; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        //SOLID RHOMBUS PATTERN
+//        x=5;
+//        for (int i = 1; i <=x ; i++) {
+//            for (int j = 1; j <=x-i ; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j <=x; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        //hollow rohmbus
+//        x=5;
+//        for (int i = 1; i <=x ; i++) {
+//            for (int j = 1; j <=x-i ; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j <=x; j++) {
+//                if(i==1||i==x||j==1||j==x)System.out.print("*");
+//                else System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+        //DIAMOND PATTERN
+        x=4;
+        for (int i = 1; i <=x; i++) {
+            for (int j = 1; j <=x-i ; j++) {
+                System.out.print(" ");
             }
-           // c='A';
+            for (int j = 1; j <2*i; j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
-        //print character patter
-         c='A';
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(c);
-                c++;
+        for (int i = x; i >=1; i--) {
+            for (int j = 1; j <=x-i ; j++) {
+                System.out.print(" ");
             }
-            c='A';
-            System.out.println(); 
+            for (int j = 1; j <2*i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
+
+
     }
 }
+        
+
+
+
